f3fe4500673a3afc8af9f6b40fdb603d174365a5
Félix Saparelli <felix@passcod.name>
2017-02-02 16:31:58 +1300

    Initial structs

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,19 @@ mod tests {
     fn it_works() {
     }
 }
+
+struct Proposal {
+    pub round: usize,
+    pub message: String,
+}
+
+struct Acceptor {
+    pub round: usize,
+    pub accepted: Vec<Proposal>,
+    pub backend: Service,
+}
+
+trait Service {
+    fn append(&mut self, path: String, message: String) -> String;
+    fn fetch(&self, path: String) -> String;
+}
